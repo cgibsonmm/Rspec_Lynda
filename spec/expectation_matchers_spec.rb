d6bc18e81fb4c407555007405af3b11de2f91a55
Corey Gibson <cgibsonmm@gmail.com>
2017-08-16 13:06:44 -0400

    finished chapt 3

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -204,4 +204,64 @@ describe 'Expectation Matchers' do
       expect(customer).to have_pending_order
     end
   end
+
+  describe 'observation matchers' do
+    it 'will match when events change object attributes' do
+      array = []
+      expect { array << 1 }.to change(array, :empty?).from(true).to(false)
+
+      class WebsiteHits
+        attr_accessor :count
+        def initialize; @count = 0; end
+        def increment; @count +=1; end
+      end
+      hits = WebsiteHits.new
+      expect { hits.increment }.to change(hits, :count).from(0).to(1)
+    end
+
+    it 'will match when events change any values' do
+      x = 10
+      expect { x += 1 }.to change {x}.from(10).to(11)
+      expect { x += 1 }.to change {x}.by(1)
+      expect { x += 1 }.to change {x}.by_at_least(1)
+      expect { x += 1 }.to change {x}.by_at_most(1)
+
+      z = 11
+      expect { z += 1 }.to change { z % 3 }.from(2).to(0)
+    end
+
+    it 'will match errors when raised' do
+      expect { raise StandardError }.to raise_error
+      expect { raise StandardError }.to raise_exception
+
+      expect { 1 / 0 }.to raise_error(ZeroDivisionError)
+      expect { 1 / 0 }.to raise_error.with_message("divided by 0")
+      expect { 1 / 0 }.to raise_error.with_message(/divided/)
+
+      expect { 1 / 1 }.not_to raise_error
+    end
+
+    it 'will match when output is generated' do
+      expect { print('hello') }.to output.to_stdout
+      expect { print('hello') }.to output('hello').to_stdout
+      expect { print('hello') }.to output(/ll/).to_stdout
+
+      expect { warn('problem') }.to output(/problem/).to_stderr
+    end
+  end
+
+  describe 'compound expectations' do
+    it 'will match using: and, or, &, |' do
+      expect([1,2,3,4]).to start_with(1).and end_with(4)
+      expect([1,2,3,4]).to start_with(1) & include(2)
+      expect(10 * 10).to be_odd.or be > 50
+
+      array = %w(hello goodbye).shuffle
+      expect(array.first).to eq('hello') | eq('goodbye')
+    end
+  end
+
+  describe 'composing matchers' do
+
+  end
 end
